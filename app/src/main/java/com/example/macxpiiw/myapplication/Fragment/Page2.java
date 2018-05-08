a4a6f025455628afdc17a5fbf374636a03b61658
macxpiiw <piiz.pumz@hotmail.com>
2018-05-08 17:56:36 +0700

    Initial commit

--- app/src/main/java/com/example/macxpiiw/myapplication/Fragment/Page2.java
+++ app/src/main/java/com/example/macxpiiw/myapplication/Fragment/Page2.java
@@ -58,7 +58,7 @@ public class Page2 extends Fragment {
 
 //        populaterecyclerView(filter);
 
-        searchView.setQueryHint("หมู่บ้าน,หมู่ที่,ตำบล,จังหวัด qw[]f kofwep;gnolgdcfghjklwgwegwg");
+        searchView.setQueryHint("หมู่บ้าน,หมู่ที่,ตำบล,จังหวัด,testGit ");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
             @Override
